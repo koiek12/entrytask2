7629ac052d267153f80d88c091a8d7057f341508
Youngiek Song <youngiek.song@shopee.com>
2020-04-06 19:18:08 +0800

    asdsad

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"os"
 
 	"git.garena.com/youngiek.song/entry_task/internal/logger"
 	"git.garena.com/youngiek.song/entry_task/pkg/message"
@@ -21,25 +20,8 @@ func initLogger() {
 	logger.Init("server.log")
 }
 
-func initDB() {
-	var err error
-	db, err = sql.Open("mysql", "song:abcd@/entry_task")
-	if err != nil {
-		logger.Instance.Fatal("Cannot connect to DB " + err.Error())
-		os.Exit(1)
-	}
-	db.SetMaxIdleConns(20)
-	err = db.Ping()
-	if err != nil {
-		logger.Instance.Fatal("Cannot connect to DB " + err.Error())
-		logger.Instance.Fatal("Cannot connect to DB")
-		os.Exit(1)
-	}
-}
-
 func main() {
 	initLogger()
-	initDB()
 
 	server := message.NewServer("localhost", "3233")
 	server.Run()
