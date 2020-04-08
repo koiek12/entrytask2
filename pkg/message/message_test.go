f69c5f1eca81084d5e61ad5e5467860d5bd5384b
youngiek.song <youngiek.song@shopee.com>
2020-04-08 12:10:31 +0800

    add MsgSteramTest for all message type

--- pkg/message/message_test.go
+++ pkg/message/message_test.go
@@ -6,8 +6,32 @@ import (
 	"time"
 )
 
-//TestMsgStream is test
-func TestMsgStream(t *testing.T) {
+func TestMsgStreamHealthCheck(t *testing.T) {
+	client, server := net.Pipe()
+	clientStream, _ := NewMsgStream(server, 60*time.Second)
+	serverStream, _ := NewMsgStream(client, 60*time.Second)
+	done := make(chan bool)
+	go func() {
+		clientStream.WriteMsg(&HealthcheckMessage{})
+		resMsg, _ := clientStream.ReadMsg()
+		_, ok := resMsg.(*HealthcheckMessage)
+		if !ok {
+			t.Fail()
+		}
+		done <- true
+	}()
+	go func() {
+		msg, _ := serverStream.ReadMsg()
+		_, ok := msg.(*HealthcheckMessage)
+		if !ok {
+			t.Fail()
+		}
+		serverStream.WriteMsg(&HealthcheckMessage{})
+	}()
+	<-done
+}
+
+func TestMsgStreamLogin(t *testing.T) {
 	client, server := net.Pipe()
 	clientStream, _ := NewMsgStream(server, 60*time.Second)
 	serverStream, _ := NewMsgStream(client, 60*time.Second)
@@ -23,7 +47,7 @@ func TestMsgStream(t *testing.T) {
 			t.Fail()
 		}
 		t.Logf("token:%s\n", logRes.Token)
-		if token != "abcd" {
+		if logRes.Token != "abcd" {
 			t.Fail()
 		}
 		done <- true
@@ -31,12 +55,131 @@ func TestMsgStream(t *testing.T) {
 	go func() {
 		msg, _ := serverStream.ReadMsg()
 		logReq, ok := msg.(*LoginRequest)
-		if !ok || logReq.Id != "hoiek12" || logReq.Password != "abc" {
+		if !ok {
 			t.Fail()
 		}
 		t.Logf("id:%s, passwd:%s\n", logReq.Id, logReq.Password)
+		if logReq.Id != "hoiek12" || logReq.Password != "abc" {
+			t.Fail()
+		}
 		serverStream.WriteMsg(&LoginResponse{
 			Token: "abcd",
+		})
+	}()
+	<-done
+}
+
+func TestMsgStreamGetUserInfo(t *testing.T) {
+	client, server := net.Pipe()
+	clientStream, _ := NewMsgStream(server, 60*time.Second)
+	serverStream, _ := NewMsgStream(client, 60*time.Second)
+	done := make(chan bool)
+	go func() {
+		clientStream.WriteMsg(&GetUserInfoRequest{
+			Token: "abcd",
+		})
+		resMsg, _ := clientStream.ReadMsg()
+		getRes, ok := resMsg.(*GetUserInfoResponse)
+		if !ok {
+			t.Fail()
+		}
+		t.Logf(getRes.String())
+		if getRes.User.Nickname != "nick" {
+			t.Fail()
+		}
+		done <- true
+	}()
+	go func() {
+		msg, _ := serverStream.ReadMsg()
+		getReq, ok := msg.(*GetUserInfoRequest)
+		if !ok {
+			t.Fail()
+		}
+		t.Logf(getReq.String())
+		if getReq.Token != "abcd" {
+			t.Fail()
+		}
+		serverStream.WriteMsg(&GetUserInfoResponse{
+			User: &User{
+				Nickname: "nick",
+			},
+		})
+	}()
+	<-done
+}
+
+func TestMsgStreamEditUserInfo(t *testing.T) {
+	client, server := net.Pipe()
+	clientStream, _ := NewMsgStream(server, 60*time.Second)
+	serverStream, _ := NewMsgStream(client, 60*time.Second)
+	done := make(chan bool)
+	go func() {
+		clientStream.WriteMsg(&EditUserInfoRequest{
+			Token: "abcd",
+			User: &User{
+				Nickname: "john",
+			},
+		})
+		resMsg, _ := clientStream.ReadMsg()
+		res, ok := resMsg.(*Response)
+		if !ok {
+			t.Fail()
+		}
+		t.Logf("code:%d", res.Code)
+		if res.Code != 0 {
+			t.Fail()
+		}
+		done <- true
+	}()
+	go func() {
+		msg, _ := serverStream.ReadMsg()
+		editReq, ok := msg.(*EditUserInfoRequest)
+		if !ok {
+			t.Fail()
+		}
+		t.Logf(editReq.String())
+		if editReq.Token != "abcd" || editReq.User.Nickname != "john" {
+			t.Fail()
+		}
+		serverStream.WriteMsg(&Response{
+			Code: 0,
+		})
+	}()
+	<-done
+}
+
+func TestMsgStreamAuthenticate(t *testing.T) {
+	client, server := net.Pipe()
+	clientStream, _ := NewMsgStream(server, 60*time.Second)
+	serverStream, _ := NewMsgStream(client, 60*time.Second)
+	done := make(chan bool)
+	go func() {
+		clientStream.WriteMsg(&AuthRequest{
+			Token: "abcd",
+		})
+		resMsg, _ := clientStream.ReadMsg()
+		res, ok := resMsg.(*Response)
+		if !ok {
+			t.Fail()
+		}
+		t.Logf("code:%d", res.Code)
+		if res.Code != 0 {
+			t.Fail()
+		}
+		done <- true
+	}()
+	go func() {
+		msg, _ := serverStream.ReadMsg()
+		authReq, ok := msg.(*AuthRequest)
+		if !ok {
+			t.Fail()
+		}
+		t.Logf(authReq.String())
+		if authReq.Token != "abcd" {
+			t.Fail()
+		}
+		serverStream.WriteMsg(&Response{
+			Code: 0,
 		})
 	}()
 	<-done
